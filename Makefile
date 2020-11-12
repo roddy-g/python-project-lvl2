b13413ae9047bc43402da302b34150d78af1bd02
Roddy <per677199@yandex.ru>
2020-11-12 19:02:26 +0300

    added test

--- Makefile
+++ Makefile
@@ -3,5 +3,6 @@ install:
 lint:
 	poetry run flake8 gendiff
 run:
-	poetry run gendiff 1.json 2.json 
-	
+	poetry run gendiff gendiff/tests/fixtures/simple_json_file_1.json gendiff/tests/fixtures/simple_json_file_2.json 
+test:
+	poetry run tests	
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -10,6 +10,11 @@ def main():
     args = parser.parse_args()
     first_file = json.load(open(args.first_file))
     second_file = json.load(open(args.second_file))
+    result = find_diffs(first_file, second_file)
+    print(result)
+
+
+def find_diffs(first_file, second_file):
     diff = []
     unique_keys = set(list(first_file.keys()) + list(second_file.keys()))
     for key in sorted(unique_keys):
@@ -25,8 +30,17 @@ def main():
             diff.append(' + {}:{}'.format(key, second_file_value))
         elif first_file_value is not None:
             diff.append(' - {}:{}'.format(key, first_file_value))
-    result = '{{\n{}\n}}'.format('\n'.join(diff))
-    print(result)
+    return '{{\n{}\n}}'.format('\n'.join(diff))
+
+
+def test(file_1='gendiff/tests/fixtures/simple_json_file_1.json',
+ file_2='gendiff/tests/fixtures/simple_json_file_2.json',
+ diff='gendiff/tests/fixtures/diff_file_1_file_2.txt'):
+    first_file = json.load(open(file_1))
+    second_file = json.load(open(file_2))
+    different_true = open(diff).read()
+    diffrenet_by_func = find_diffs(first_file, second_file)
+    assert diffrenet_by_func == different_true
 
 
 if __name__ == '__main__':
