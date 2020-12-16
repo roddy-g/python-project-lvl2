b465dca95fdfe2a529b273169db3ee0f6be9bba4
Roddy <per677199@yandex.ru>
2020-12-16 10:54:52 +0300

    refator diff for simple files

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,6 +1,9 @@
 import argparse
 from gendiff.data_loader import load_data
 
+INDENT_IF_UNCNAHGED = '    '
+INDENT_IF_DELETED = '  - '
+INDENT_IF_ADDED = '  + '
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff.')
@@ -11,54 +14,61 @@ def main():
     first_file_data = load_data(args.path_to_first_file)
     second_file_data = load_data(args.path_to_second_file)
     diff = generate_diff(first_file_data, second_file_data)
-    return diff
+    return stylish(diff)
 
 
 def generate_diff(first_file_data, second_file_data):
     diff = {}
-    first_file_keys = set()
-    second_file_keys = set()
-    if type(first_file_data) is dict:
-        first_file_keys = set(first_file_data.keys())
-    if type(second_file_data) is dict:
-        second_file_keys = set(second_file_data.keys())
-    common_keys = first_file_keys.intersection(second_file_keys)
-    changed_keys = set(key for key in common_keys if first_file_data[key]
-                       != second_file_data[key])
-    unchanged_keys = common_keys - changed_keys
-    deleted_keys = first_file_keys.difference((second_file_keys))
-    added_keys = second_file_keys.difference(first_file_keys)
-    for key in unchanged_keys:
-        diff[key] = {'status': 'unchanged', 'value': first_file_data.get(key)}
-    for key in changed_keys:
-        diff[key] = {'status': 'changed',
-                     'value_before': first_file_data.get(key),
-                     'value_after': second_file_data.get(key)}
-    for key in deleted_keys:
-        diff[key] = {'status': 'deleted', 'value': first_file_data.get(key)}
-    for key in added_keys:
-        diff[key] = {'status': 'added', 'value': second_file_data.get(key)}
+    for key in first_file_data:
+        if key not in second_file_data:
+            diff[key] = {'status': 'deleted',
+                         'value': first_file_data[key]}
+            continue
+        elif first_file_data[key] == second_file_data[key]:
+            diff[key] = {'status': 'unchanged',
+                         'value': first_file_data[key]}
+            continue
+        elif first_file_data[key] != second_file_data[key]:
+            if first_file_data[key] is dict and second_file_data is dict:
+                diff_child = generate_diff(first_file_data[key], second_file_data[key])
+                diff[key] = {'status': 'changed',
+                             'child_diff_value': diff_child}
+            else:
+                diff[key] = {'status': 'changed',
+                             'value_before': first_file_data[key],
+                             'value_after': second_file_data[key]}
+    for key in second_file_data:
+        if key not in first_file_data:
+            diff[key] = {'status': 'added',
+                         'value': second_file_data[key]}
     print(stylish(diff))
     return stylish(diff)
 
 
-def stylish(diff):
+def stylish(diff, level=0):
     result = []
+    indent = '    ' * level
     for key in sorted(diff.keys()):
         if diff[key]['status'] == 'unchanged':
-            result.append('   {}:{}'.format(key, diff[key]['value']))
+            result.append('{}{}{}:{}'.format(indent, INDENT_IF_UNCNAHGED, key, diff[key]['value']))
         elif diff[key]['status'] == 'changed':
-            result.append(' - {}:{}'.format(key, diff[key]['value_before']))
-            result.append(' + {}:{}'.format(key, diff[key]['value_after']))
+            if diff[key].get('child_diff_value'):
+                stylish(diff[key]['child_diff_value'], level=level+1)
+            else:
+                result.append('{}{}{}:{}'.format(indent, INDENT_IF_DELETED, key, diff[key]['value_before']))
+                result.append('{}{}{}:{}'.format(indent, INDENT_IF_ADDED, key, diff[key]['value_after']))
         elif diff[key]['status'] == 'deleted':
-            result.append(' - {}:{}'.format(key, diff[key]['value']))
+            result.append('{}{}{}:{}'.format(indent, INDENT_IF_DELETED, key, diff[key]['value']))
         elif diff[key]['status'] == 'added':
-            result.append(' + {}:{}'.format(key, diff[key]['value']))
-    result.insert(0, '{')
-    result.append('}')
+            result.append('{}{}{}:{}'.format(indent, INDENT_IF_ADDED, key, diff[key]['value']))
+    wrap(result)
     formatted_diff = '\n'.join(result)
     return formatted_diff
 
+
+def wrap(data):
+    data.insert(0, '{')
+    data.append('}')
 
 if __name__ == '__main__':
     main()
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,7 +2,7 @@ from gendiff.gendiff import generate_diff
 from gendiff.data_loader import load_data
 
 
-def test():
+def test_simple_files():
     correct_diff = open('tests/fixtures/correct_diff_for_simple_files.txt').read()
     first_file_data = load_data('tests/fixtures/simple_json_file_1.json')
     second_file_data = load_data('tests/fixtures/simple_json_file_2.json')
@@ -12,6 +12,14 @@ def test():
     second_file_data = load_data('tests/fixtures/simple_yml_file_2.yml')
     assert generate_diff(first_file_data, second_file_data) == correct_diff
     print("simple yml test passed")
+
+
+def test_complicated_files():
+    correct_diff = open('tests/fixtures/correct_diff_for_simple_files.txt').read()
+    first_file_data = load_data('tests/fixtures/complicated_json_file_1.json')
+    second_file_data = load_data('tests/fixtures/complicated_json_file_2.json')
+    assert generate_diff(first_file_data, second_file_data) == correct_diff
+    print("complicated json test passed")
 
 
 if __name__ == '__main__.py':
