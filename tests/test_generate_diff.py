5a61e6f20c6adfe9686c0fb6101f1ea5cffc1d48
Roddy <per677199@yandex.ru>
2021-01-11 10:27:09 +0300

    refactoring tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,19 +2,19 @@ from gendiff import generate_diff
 import pytest
 
 
-simple_fixtures_list = [
+fixtures_list = [
     (
         'tests/fixtures/simple/json_file_1.json',
         'tests/fixtures/simple/json_file_2.json',
-        'tests/fixtures/simple/correct_diff_stylish.txt'
+        'tests/fixtures/simple/correct_diff_stylish.txt',
+        ''
     ),
     (
         'tests/fixtures/simple/yml_file_1.yml',
         'tests/fixtures/simple/yml_file_2.yml',
-        'tests/fixtures/simple/correct_diff_stylish.txt'
-    )
-]
-complicated_fixtures_list = [
+        'tests/fixtures/simple/correct_diff_stylish.txt',
+        ''
+    ),
     (
         'tests/fixtures/complicated/yml_file_1.yml',
         'tests/fixtures/complicated/yml_file_2.yml',
@@ -37,17 +37,9 @@ complicated_fixtures_list = [
 ]
 
 
-@pytest.mark.parametrize('source,changed_source,correct_diff_path',
-                         simple_fixtures_list)
-def test_simple_files(source, changed_source, correct_diff_path):
-    diff = generate_diff(source, changed_source)
-    with open(correct_diff_path, 'r') as correct_diff:
-        assert diff == correct_diff.read()
-
-
 @pytest.mark.parametrize('source,changed_source,correct_diff_path,style',
-                         complicated_fixtures_list)
-def test_complicated_files(source, changed_source, correct_diff_path, style):
+                         fixtures_list)
+def test_generate_diff(source, changed_source, correct_diff_path, style):
     if style:
         diff = generate_diff(source, changed_source, style)
     else:
