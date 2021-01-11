c1665ae538307d19224a47efa847b742a1aed62d
Roddy <per677199@yandex.ru>
2021-01-11 15:48:24 +0300

    refactoring

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -4,9 +4,9 @@ from json import load as json_load
 
 def load_data(path_to_file):
     with open(path_to_file) as data:
-        if path_to_file[-4:] == 'json':
+        if path_to_file[-5:] == '.json':
             data = json_load(data)
             return data
-        if path_to_file[-3:] == 'yml' or path_to_file[-4:] == 'yaml':
+        if path_to_file[-4:] == '.yml' or path_to_file[-5:] == '.yaml':
             data = yaml_load(data)
             return data
